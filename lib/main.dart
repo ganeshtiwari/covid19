a85615eefab9d2c7d31eb396d5ff0dddd5534b65
ganeshtiwari <blinkganesh@gmail.com>
2020-04-07 13:32:10 +0545

    removing charts
    Charts removed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:covid19/screens/covid_chart.dart';
 import 'package:covid19/screens/summary_screen.dart';
 import 'package:covid19/screens/test.dart';
-import 'package:flutter/material.dart'; 
-import "package:charts_flutter/flutter.dart" as charts;
+import 'package:flutter/material.dart';
 
 
 void main() => runApp(MyApp()); 
@@ -18,12 +17,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.purple[800],
         accentColor: Colors.cyan[600]
       ),
-      initialRoute: CovidChart.id,
-      routes: {
-        SummaryScreen.id: (context) => SummaryScreen(), 
-        CovidChart.id: (context) => CovidChart(),
-        Test.id: (context) => Test(),
-      },
+      home: SummaryScreen(),
     );
   }
   
